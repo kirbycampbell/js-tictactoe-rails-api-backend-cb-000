aafb89dc2eea128c72abcb1ff5309d00dab87c50
Kirby Campbell <jkirbycampbell@gmail.com>
2018-10-30 20:04:07 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,20 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
+  def index
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
 
 end
